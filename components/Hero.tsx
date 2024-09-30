0c9172416aed7f814b7db55217dc4737e814c605
yukinouee <yuki2023@suou.waseda.jp>
2024-09-30 20:18:02 +0900

    Added Text Generate Effect.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "@/components/Spotlight";
+import { TextGenerateEffect } from "./TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -26,6 +27,14 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Experiences."
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Yuki, a Next.js Developer based in Japan.
+          </p>
         </div>
       </div>
     </div>
